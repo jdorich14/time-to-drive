6c344e16427979cd84c68f57709fe6c212509789
Dan <bouffard.daniel25@gmail.com>
2016-01-28 22:16:06 -0500

    Add jsoncpp setup to setup.sh. Also create cleanup.sh to remove files created by setup.sh.

--- setup.sh
+++ setup.sh
@@ -9,7 +9,24 @@ do
     sudo apt-get install $dep
 done
 
+# Make directories
+mkdir -p test/lib
+mkdir lib
+mkdir include
+
+# Google Test setup
 git clone https://github.com/google/googletest.git test/googletest
 make -C test/googletest/googletest/make gtest.a
-mkdir test/lib
 cp test/googletest/googletest/make/gtest.a test/lib
+
+# JsonCpp setup
+git clone https://github.com/open-source-parsers/jsoncpp.git
+mkdir jsoncpp/build
+cd jsoncpp/build
+cmake -DCMAKE_BUILD_TYPE=debug -DBUILD_STATIC_LIBS=on -DBUILD_SHARED_LIBS=off -DARCHIVE_INSTALL_DIR=. -G "Unix Makefiles" ..
+make
+cd ../..
+cp jsoncpp/build/src/lib_json/libjsoncpp.a lib
+cp -r jsoncpp/include/json include
+rm -rf jsoncpp
+pwd
